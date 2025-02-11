0702888781e4eb878214254f928c4e65995054f6
Ameya Joshi <ameyasureshjoshi@gmail.com>
2025-02-11 17:45:08 -0600

    Followed how to do file co-location, private folders and route groups

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import LineChart from './line-chart';
+
+const BarChart = () => {
+  return <h1>Bar Chart</h1>;
+};
+
+const Page = () => {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <LineChart />
+    </>
+  );
+};
+
+export default Page;
